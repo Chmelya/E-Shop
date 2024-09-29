7bea39884f49cac0e323afabb41675913e53e5c6
Dzmitry <chmel.dmitriy@mail.ru>
2024-09-29 19:44:42 +0300

    reordered

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,23 @@
-import './App.scss';
+import classes from './App.module.scss';
+import { BreadCrumb } from './components/BreadCrumb/BreadCrumb';
 import { Header } from './components/Header/Header';
 import { CatalogPage } from './pages/CatalogPage/CatalogPage';
+// import { ProductPage } from './pages/ProductPage/ProductPage';
 
 function App() {
 	return (
-		<div>
+		<>
 			<Header />
-			<CatalogPage />
-		</div>
+			<div className={classes['page-container']}>
+				<BreadCrumb />
+				<div>
+					<div className={classes['catalog-container']}>
+						<CatalogPage />
+						{/* <ProductPage /> */}
+					</div>
+				</div>
+			</div>
+		</>
 	);
 }
 
---
+++ client/src/pages/ProductPage/ProductPage.tsx
@@ -0,0 +1,6 @@
+import classes from './ProductPage.module.scss';
+import { BreadCrumb } from '../../components/BreadCrumb/BreadCrumb';
+
+export const ProductPage = () => {
+	return <></>;
+};
